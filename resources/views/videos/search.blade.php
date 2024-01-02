64145c2fc766c9d1bfe89a9c4a313a56daefb581
RickCraco <riccardocracolici43@gmail.com>
2024-01-02 18:38:59 +0100

    fixato stile pagina search

--- resources/views/videos/search.blade.php
+++ resources/views/videos/search.blade.php
@@ -5,30 +5,36 @@
 @section('content')
 
 <main>
-        <div class="row m-0 flex-column">
-            @foreach ($results as $result)
-                <div class="col-3 my-4 d-flex" style="width: 323px; height: 274px;"><a href="" style="text-decoration: none">
-                    <a href="{{route('videos.show', $result->id)}}" style="text-decoration: none">
-                        <div><img src="{{ $result->thumb }}" alt="" style="width: 360px; height: 201px;" class="rounded-4"></div>
+    <div class="row m-0">
+    <div class="d-flex col-12">
+    <div class="d-flex flex-column">
+    <h2 class="m-2 text-white">Video per {{$query}}</h2>
+    @foreach ($results as $result)
+                <div class="m-2">
+                <a href="" style="text-decoration: none">
                         <div class="d-flex">
-                            <img src="{{ $result->user_thumb }}" alt="" class="rounded-circle"
-                                style="width: 36px; height:36px; margin-top: 12px; margin-right: 12px">
-                            <div style="margin-top: 12px">
-                                <h2 class="text-white" style="font-size: 16px">{{ $result->title }}</h2>
-                                <h4 style="font-size: 14px; color: #AAAA;" class="m-0">{{ $result->username }}</h4>
-                                <div>
-                                    <span style="font-size: 14px; color: #AAAA;">{{ $result->views }} visualizzazioni</span>
-                                    <span style="color: #AAAA">-</span>
-                                    <span style="font-size: 14px; color: #AAAA;">
+                            <div class="">
+                                <a href="{{route('videos.show', $result->id)}}" style="text-decoration: none">
+                                <img src="{{ $result->thumb }}" alt="" style="width: 360px; height: 201px;" class="rounded-3">
+                            </div>
+                            <div class="px-2">
+                                <div class="fw-bold text-white" style="font-size: 18px">{{ $result->title }}</div>
+                                <div style="font-size: 14px; color: #AAAA;" class="d-flex align-items-center">
+                                    <img src="{{ $result->user_thumb }}" alt="{{ $result->username }}" class="rounded-circle me-2" style="width: 24px; height: 24px;">
+                                    {{ $result->username }}
+                                </div>
+                                <div style="font-size: 11px; color: #AAAA;" class="mt-1">{{ $result->description}}</div>
+                                <div class="">
+                                    <span style="font-size: 13px; color: #AAAA;">{{ $result->views }} Views</span>
+                                    <span style="color: #AAAA; font-size: 3px" class="align-middle"><i class="fa-solid fa-circle"></i></span>
+                                    <span style="font-size: 13px; color: #AAAA;">
                                         @php
                                             $dataUscita = $result->date;
                                             $dataUscita = new DateTime($dataUscita);
                                             $now = new DateTime();
                                             $differenza = $now->diff($dataUscita);
                                             $differenzaFormattata = '';
-                                            if ($differenza->y >= 1 && $differenza->m >= 1) {
-                                                $differenzaFormattata .= $differenza->y . ' anni ' . $differenza->m . ' mesi fa';
-                                            } elseif ($differenza->y >= 1) {
+                                            if ($differenza->y >= 1) {
                                                 $differenzaFormattata .= $differenza->y . ' anni fa';
                                             } elseif ($differenza->m >= 1) {
                                                 $differenzaFormattata .= $differenza->m . ' mesi fa';
@@ -47,8 +53,11 @@
                     </a>
                 </div>
             @endforeach
-        </div>
-    </main>
+    </div>
+    </div>
+    </div>
+
+</main>
 
 
 @endsection
