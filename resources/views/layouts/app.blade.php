b6d424dc788b8fd253af4a4006e29e062a84dc0a
RickCraco <riccardocracolici43@gmail.com>
2024-01-09 15:25:24 +0100

    added fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel base @yield('title','template')</title>
+    <title>Youtube @yield('title','template')</title>
+    <link rel="icon" href="https://www.youtube.com/s/desktop/375de707/img/favicon.ico">
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
